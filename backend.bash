7e464cef798d7da83870d91fde9b72a8b95c76cf
yahplatinumproductions-design <yahplatinumproductions@gmail.com>
2026-01-18 20:05:29 +0200

    Add files via upload

---
+++ backend.bash
@@ -0,0 +1,7 @@
+cd backend
+git init
+git add .
+git commit -m "Initial backend setup"
+git branch -M main
+git remote add origin https://github.com/YOUR_USERNAME/pureroll-backend.git
+git push -u origin main
